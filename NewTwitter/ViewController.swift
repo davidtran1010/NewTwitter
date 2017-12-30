424024985a0ba7e976ba35066cc94aa878e3575e
DavidTran <davidtran@DavidTrans-MacBook-Pro.local>
2017-12-30 16:33:50 +0700

    Initial Commit

---
+++ NewTwitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewTwitter
+//
+//  Created by DavidTran on 12/30/17.
+//  Copyright © 2017 DavidTran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
